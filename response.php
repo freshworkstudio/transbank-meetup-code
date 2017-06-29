f217149858f3d690446c4381da2b1682818c74f7
Gonzalo De Spirito <gonzunigad@gmail.com>
2017-06-29 15:21:33 -0400

    [Response] Add transaction acknowledge and return

--- response.php
+++ response.php
@@ -10,4 +10,14 @@ $bag = CertificationBagFactory::integrationWebpayNormal();
 $plus = TransbankServiceFactory::normal($bag);
 
 $response = $plus->getTransactionResult();
-echo '<pre>' . print_r($response, true) . '</pre>';
+//echo '<pre>' . print_r($response, true) . '</pre>';
+
+// Verificar stock, que el monto corresponda, etc...
+$plus->acknowledgeTransaction();
+
+//Usted no lo haga...
+$_SESSION['buyOrder'] = $response->buyOrder;
+
+//Redirect back to Webpay Flow and then to the thanks page
+return RedirectorHelper::redirectBackNormal($response->urlRedirection);
+
