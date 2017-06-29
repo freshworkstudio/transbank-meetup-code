797688b34d9e000827a2daa3630d22d48566e913
Gonzalo De Spirito <gonzunigad@gmail.com>
2017-06-29 16:39:10 -0400

    [Finish] Get variables from session. Dont doit

--- finish.php
+++ finish.php
@@ -1,13 +1,11 @@
 <?php
-
 session_start();
-use Freshwork\Transbank\CertificationBagFactory;
-use Freshwork\Transbank\TransbankServiceFactory;
 
-include 'vendor/autoload.php';
+$response = json_decode($_SESSION['tbkResponse']);
+//echo '<pre>' . print_r($response, true) . '</pre>';
 
-$bag = CertificationBagFactory::integrationWebpayNormal();
-$plus = TransbankServiceFactory::normal($bag);
-
-$response = $plus->getTransactionResult();
-echo '<pre>' . print_r($response, true) . '</pre>';
+if ($response->detailOutput->responseCode === 0) {
+    echo "<h1>Gracias por tu compra</h1>";
+} else {
+    echo "<h1>La transacción ha sido rechazada</h1><h3>Las posibles causas de este rechazo son:</h3><p>- Error en el ingreso de los datos de su tarjeta de Crédito o Débito (fecha y/o código de seguridad).<br>- Su tarjeta de Crédito o Débito no cuenta con saldo suficiente</p>";
+}
