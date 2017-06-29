5941e6b1a507eadcbb8983eb660fc3c39b515c01
Gonzalo De Spirito <gonzunigad@gmail.com>
2017-06-29 16:37:05 -0400

    [Finish] Add demo code

--- finish.php
+++ finish.php
@@ -1,1 +1,13 @@
 <?php
+
+session_start();
+use Freshwork\Transbank\CertificationBagFactory;
+use Freshwork\Transbank\TransbankServiceFactory;
+
+include 'vendor/autoload.php';
+
+$bag = CertificationBagFactory::integrationWebpayNormal();
+$plus = TransbankServiceFactory::normal($bag);
+
+$response = $plus->getTransactionResult();
+echo '<pre>' . print_r($response, true) . '</pre>';
