cd6db86571be155d9576eb2f63621a707f9446df
Gonzalo De Spirito <gonzunigad@gmail.com>
2017-06-29 15:09:15 -0400

    [Response] Add basic response code

--- response.php
+++ response.php
@@ -1,1 +1,13 @@
 <?php
+
+use Freshwork\Transbank\CertificationBagFactory;
+use Freshwork\Transbank\TransbankServiceFactory;
+use Freshwork\Transbank\RedirectorHelper;
+
+include 'vendor/autoload.php';
+
+$bag = CertificationBagFactory::integrationWebpayNormal();
+$plus = TransbankServiceFactory::normal($bag);
+
+$response = $plus->getTransactionResult();
+echo '<pre>' . print_r($response, true) . '</pre>';
