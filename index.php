b9cd63aace5795a679c339eb35ed63d2467d2d32
Gonzalo De Spirito <gonzunigad@gmail.com>
2017-06-29 14:45:06 -0400

    [Home] Add basic home

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Transbank Demo</title>
+
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+        .btn {
+            padding: 10px;
+            margin: 10px;
+            border:none;
+            text-decoration: none;
+            color: #333;
+            background: #ccc;
+            border-radius: 3px;
+        }
+        .btn:hover {
+            background: #eee;
+            transform: scale(0.9);
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Compra en este increible sitio</h1>
+    <a class="btn" href="pay.php">¡Obvio! ¡Pagar $10.000 ahora!</a>
+</body>
+</html>
